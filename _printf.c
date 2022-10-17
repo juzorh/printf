713291307617ba50a127e4d6cdae55a9d3096445
juzorh <juzor09@gmail.com>
2022-10-17 14:48:25 -0700

    tasks

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 		{"u", unsigned_integer},
 		{"o", print_octal},
 		{"x", print_hex},
-		{"X", print_heX},
+		{"X", print_HEX},
 		{NULL, NULL}
 	};
 	va_list arg_list;
